0e57e2768a2c874ff250f7c54858d94c00bbebee
Kira Smith <kira.smith@accrisoft.com>
2025-11-24 13:32:58 -0500

    Chore: Bump dependencies

--- dist/valueData.d.ts
+++ dist/valueData.d.ts
@@ -1,15 +1,15 @@
 import { PrimitiveTypeString } from './typeDetection';
 import { IxInspectContext } from './context';
-export declare type ValueDataPrim = {
+export type ValueDataPrim = {
     typeName: PrimitiveTypeString;
     isPrim: true;
 };
-export declare type ValueDataNonPrim = {
+export type ValueDataNonPrim = {
     typeName: string;
     isPrim: false;
     toStringTagValue: string;
     getPrefix: (size?: string) => string;
 };
-export declare type ValueData = ValueDataPrim | ValueDataNonPrim;
+export type ValueData = ValueDataPrim | ValueDataNonPrim;
 export declare function createValueData(value: unknown, ctx: IxInspectContext): ValueData;
 //# sourceMappingURL=valueData.d.ts.map
